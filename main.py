eac8bbff4837857578b995523194b4035e27ae71
JMTing <jason.m.ting@gmail.com>
2014-11-26 18:44:12 -0600

    fixed separating the dfs

--- main.py
+++ main.py
@@ -42,17 +42,35 @@ TrainMatrix = trainingreviews.merge(business,on="business_id")
 TrainMatrix = TrainMatrix.merge(users,on="user_id", how='left')
 TestMatrix = testreviews.merge(business,on="business_id", how='left')
 TestMatrix = TestMatrix.merge(users,on="user_id", how='left')
-XTrain, YTrain = features.not_so_quick_train(TrainMatrix)
-XTest = features.not_so_quick_test(TestMatrix, TrainMatrix)
+# XTrain, YTrain = features.not_so_quick_train(TrainMatrix)
+# XTest = features.not_so_quick_test(TestMatrix, TrainMatrix)
 
 # splits the dataframe depending on what is missing
-missing_both_df, missing_business_df, missing_user_df, missing_none_df = features.separate_df(TestMatrix)
+df_index = TestMatrix.index.values.tolist()
+business_index = pd.isnull(TestMatrix['bus_stars']).tolist()
+business_index = [i for i, elem in enumerate(business_index) if elem]
+user_index = pd.isnull(TestMatrix['user_average_stars']).tolist()
+user_index = [i for i, elem in enumerate(user_index) if elem]
+
+# finds the indices depending on what is missing
+missing_both_index = list(set(business_index) & set(user_index))
+missing_user_index = list(set(user_index) - set(business_index))
+missing_business_index = list(set(business_index) - set(user_index))
+missing_none = list(set(df_index) - set(business_index) - set(user_index))
+
+missing_both_df = TestMatrix.iloc[missing_both_index,:]
+missing_business_df = TestMatrix.iloc[missing_business_index, :]
+missing_user_df = TestMatrix.iloc[missing_user_index, :]
+missing_none_df = TestMatrix.iloc[missing_none, :]
+print missing_business_df.head(2)
 
 # machine learning aka CS229 
 # splits for now-in the future we need to make the test matrix from the data
+'''
 clf = linear_model.LinearRegression().fit(XTrain, YTrain)
 results = pd.DataFrame(clf.predict(XTest), index = TestMatrix.review_id.values, columns = ['stars'])
 results.to_csv('submission.csv')
+'''
 
 # print "RMSE: %.2f" % np.sqrt(np.mean((clf.predict(xtest) - ytest) ** 2))
 
